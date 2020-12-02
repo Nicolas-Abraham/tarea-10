cf28568e9c806f585a7fa8f86319dea58e1d2310
Nicolas-Abraham <73892915+Nicolas-Abraham@users.noreply.github.com>
2020-12-02 15:33:34 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.B, function () {
-    if (input.compassHeading() > 23) {
+    if (input.lightLevel() > 23) {
         music.playTone(392, music.beat(BeatFraction.Double))
     } else {
         music.playMelody("G D B C5 A C5 E G ", 120)
